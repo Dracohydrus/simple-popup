44b677ebce0ad849f8feb2663573888f8cb2b7b3
Dracohydrus <dracokris123@gmail.com>
2022-02-07 14:36:22 -0500

    Finished popup functionality
    - Used a promise to return to the main html button to choose what we want to do after the promise is resolved

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 const clickMeButton = document.getElementById("button1")
 const content = document.querySelector("[data-content]")
 
-const createPopup = (message, actions=["Yes", "No"]) => {
-    const popup = document.querySelector("[data-popup]")
-    const popupFragment = popup.content.cloneNode(true).children[0]
-
-    const closeButton = popupFragment.querySelector("[data-close-button]")
-    closeButton.addEventListener("click",(e) => {
-        popupFragment.remove()
-    })
-
-    const messageComponent = popupFragment.querySelector("[data-message]")
-    const actionsComponent = popupFragment.querySelector("[data-actions]")
-    messageComponent.textContent = message;
-    actions.forEach((action) => {
-        actionsComponent.innerHTML += `<button data-button-${action}>${action}</button>`
-    })
-    actions.forEach((action) => {
-        const newButton = actionsComponent.querySelector(`[data-button-${action}]`)
-        newButton.addEventListener("click", (e) => {
-            console.log(e.target.innerHTML)
+const createPopup = (popup, message, actions=["Yes", "No"]) => {
+    var promise = new Promise((resolve, reject) => {
+        const popupFragment = popup.content.cloneNode(true).children[0]
+        const closeButton = popupFragment.querySelector("[data-close-button]")
+        closeButton.addEventListener("click",(e) => {
+            resolve(null);
             popupFragment.remove()
         })
-    })
     
-    content.append(popupFragment)
+        const messageComponent = popupFragment.querySelector("[data-message]")
+        const actionsComponent = popupFragment.querySelector("[data-actions]")
+        messageComponent.textContent = message;
+        actions.forEach((action) => {
+            actionsComponent.innerHTML += `<button data-button-${action}>${action}</button>`
+        })
+        actions.forEach((action) => {
+            const newButton = actionsComponent.querySelector(`[data-button-${action}]`)
+            newButton.addEventListener("click", (e) => {
+                resolve(e.target.innerHTML)
+                popupFragment.remove()
+            })
+        })
+        content.append(popupFragment)
+    })
+    return promise;
 }
 
-
 clickMeButton.addEventListener("click", (e) => {
-    createPopup("Test message new", ["proceed", "cancel", "other"])
+    const popup = document.querySelector("[data-popup]")
+    createPopup(popup, "Test message new", ["proceed", "cancel", "other"])
+    .then((resolve) => {
+        console.log(resolve)
+    })
 })
 
